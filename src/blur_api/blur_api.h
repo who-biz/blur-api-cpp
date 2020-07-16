073356221292e2d41988ce04d3190df2841cec77
who-biz <37732338+who-biz@users.noreply.github.com>
2020-07-16 14:04:47 -0400

    Add init function, remove BlurAPI class from stubserver.cpp

--- src/blur_api/blur_api.h
+++ src/blur_api/blur_api.h
@@ -13,7 +13,10 @@ public:
     /* === Constructor and Destructor === */
     BlurAPI(const std::string& user, const std::string& password, const std::string& host, int port, int httpTimeout = 500);
     BlurAPI* m_blur_api;
+    getinfo_t getblockchaininfo();
     BlurAPI get_blur_api();
     ~BlurAPI();
     Json::Value sendcommand(std::string const& command, Json::Value const& params);
 };
+
+BlurAPI blur_api_init();

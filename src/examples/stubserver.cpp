e0b7c537d624f5fbe648fb7afe8d4b3681777473
who-biz <37732338+who-biz@users.noreply.github.com>
2020-10-23 14:32:34 -0400

    Add ten duplicates to 'listunspent' entries, increment vout each

--- src/examples/stubserver.cpp
+++ src/examples/stubserver.cpp
@@ -116,10 +116,14 @@ Json::Value MyStubServer::listunspent(int const minconf, int const maxconf, Json
   }
   Json::Value blurapi_result = m_blur_api->listunspent(minconf, maxconf, addr_list);
   Json::Value result, itemone, itemtwo;
+  uint16_t vout = 0;
   for (auto& each : blurapi_result["entries"]) {
-    each["amount"] = 0.0001;
-    each["generated"] = false;
-    result.append(each);
+    for (size_t n = 0; n < 10; n++) {
+      each["amount"] = 0.00010000;
+      each["vout"] = vout;
+      vout++;
+      result.append(each);
+    }
   }
   return result;
 }
